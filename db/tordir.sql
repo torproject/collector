09c81f14ab01983c7a721dade8aded1f775727b6
Karsten Loesing <karsten.loesing@gmx.net>
2011-02-06 12:08:14 +0100

    Update index definitions.

--- db/tordir.sql
+++ db/tordir.sql
@@ -115,16 +115,13 @@ CREATE TABLE connbidirect (
 );
 
 -- Create the various indexes we need for searching relays
-CREATE INDEX statusentry_validafter_address
-  ON statusentry (validafter, address);
-CREATE INDEX statusentry_descriptor ON statusentry (descriptor);
-CREATE INDEX statusentry_validafter_fingerprint
-  ON statusentry (validafter, fingerprint);
-CREATE INDEX statusentry_validafter_nickname
-  ON statusentry (validafter, LOWER(nickname));
+CREATE INDEX statusentry_address ON statusentry (address);
+CREATE INDEX statusentry_fingerprint ON statusentry (fingerprint);
+CREATE INDEX statusentry_nickname ON statusentry (LOWER(nickname));
 CREATE INDEX statusentry_validafter ON statusentry (validafter);
 
 -- And create an index that we use for precalculating statistics
+CREATE INDEX statusentry_descriptor ON statusentry (descriptor);
 CREATE INDEX statusentry_validafter_date ON statusentry (DATE(validafter));
 
 -- TABLE network_size
